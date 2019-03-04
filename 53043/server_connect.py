83a1b4ae21baa34eb7de72fef2333b89f68faca7
Abhijeet Kasurde <akasurde@redhat.com>
2019-03-04 15:37:16 +0530

    Debug 53043
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

---
+++ 53043/server_connect.py
@@ -0,0 +1,41 @@
+from pyVim.connect import SmartConnect, Disconnect
+import ssl
+import atexit
+from pyVmomi import vim, vmodl, VmomiSupport
+import time
+
+
+def connect(hostname, username, password):
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    context.verify_mode = ssl.CERT_NONE
+
+    si = SmartConnect(host=hostname, user=username, pwd=password, port=443, sslContext=context)
+    atexit.register(Disconnect, si)
+    content = si.RetrieveContent()
+    return content
+
+def get_obj(content, vimtype, name):
+    """
+    Return an object by name, if name is None the
+    first found object is returned
+    """
+    obj = None
+    if name in ['', None]:
+        obj = []
+    container = content.viewManager.CreateContainerView(content.rootFolder, vimtype, True)
+    for c in container.view:
+        if name:
+            if c.name == name:
+                obj = c
+                break
+        else:
+            obj.append(c)
+
+    container.Destroy()
+    return obj
+
+hostname = '10.65.223.62'
+username = 'administrator@vsphere.local'
+password = 'Esxi@123$%'
+content = connect(hostname, username, password)
+
