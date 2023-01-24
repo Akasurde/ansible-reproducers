fe162e18d89b0d997c85a03d9474547795fef82a
Abhijeet Kasurde <akasurde@redhat.com>
2023-01-24 08:14:04 +0000

    Extra var examples

---
+++ ansible-sdk-extravars/extravars_multiple_host_multiple_vars.py
@@ -0,0 +1,23 @@
+import asyncio
+
+
+from ansible_sdk.executors import AnsibleSubprocessJobExecutor, AnsibleSubprocessJobOptions
+from example_common import run_one_stdout
+
+
+async def main():
+    executor = AnsibleSubprocessJobExecutor()
+    executor_options = AnsibleSubprocessJobOptions()
+
+    job_options = {
+        'playbook': 'extravars_multiple_host_multiple_vars.yml',
+        'extra_vars': {
+            'extra_variable': '1',
+            'another_variable': '2',
+        },
+    }
+    await run_one_stdout(executor, executor_options, job_options=job_options)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ ansible-sdk-extravars/extravars_single_host_single_var.py
@@ -0,0 +1,22 @@
+import asyncio
+
+
+from ansible_sdk.executors import AnsibleSubprocessJobExecutor, AnsibleSubprocessJobOptions
+from example_common import run_one_stdout
+
+
+async def main():
+    executor = AnsibleSubprocessJobExecutor()
+    executor_options = AnsibleSubprocessJobOptions()
+
+    job_options = {
+        'playbook': 'extravars_single_host_single_var.yml',
+        'extra_vars': {
+            'extra_variable': '1',
+        },
+    }
+    await run_one_stdout(executor, executor_options, job_options=job_options)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
