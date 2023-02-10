f3aee9edbd63959822a1ddea599b4510f8a451c8
Abhijeet Kasurde <akasurde@redhat.com>
2023-02-10 06:44:27 +0000

    Update encrypted_role script
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- ansible-sdk-vault-e2e/example_encrypted_role.py
+++ ansible-sdk-vault-e2e/example_encrypted_role.py
@@ -11,13 +11,20 @@ async def main():
     executor = AnsibleSubprocessJobExecutor()
     executor_options = AnsibleSubprocessJobOptions()
 
-    with open(os.path.join(os.getcwd(), 'datadir', 'env', 'cmdline'), 'w') as f:
+    datadir = os.path.join(os.getcwd(), "datadir")
+    env_dir = os.path.join(datadir, 'env')
+    roles_path = os.path.join(os.getcwd(), "roles")
+
+    if not os.path.exists(env_dir):
+        os.mkdirs(env_dir)
+
+    with open(os.path.join(env_dir, 'cmdline'), 'w') as f:
         f.write("--vault-password-file=./get_password.sh")
 
     job_options = {
-        'datadir': os.path.join(os.getcwd(), "datadir"),
+        'datadir': datadir,
         'role': 'enc_role',
-        'roles_path': os.getcwd() + "/roles",
+        'roles_path': roles_path,
     }
     await run_one_stdout(executor, executor_options, job_options=job_options)
 
